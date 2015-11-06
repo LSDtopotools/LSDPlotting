6dbe0ad745996e3fe33621bda0df3df05e8cb41c
simon-m-mudd <simon.m.mudd@ed.ac.uk>
2015-11-06 16:02:11 +0000

    Updated the plotting, now does 4 subplots

--- CRNResults.py
+++ CRNResults.py
@@ -37,9 +37,12 @@ class CRNResults(object):
             
             # get rid of the header
             del lines[0:3]
+
+            # get rid of the control characters
+            this_line = LSDOst.RemoveEscapeCharacters(lines[0])
             
-            # Now get a list with the names of the parameters
-            self.VariableList = lines[0].split(',')
+            # Now get a list with the names of the parameters                        
+            self.VariableList = this_line.split(',')
             
             # get rid of the names
             del lines[0]             
@@ -61,7 +64,9 @@ class CRNResults(object):
                         DataDict[name].append(split_line[index])
                     else:
                         DataDict[name].append(float(split_line[index]))
-                    print "Name is: " + name
+                    
+                    #if name == 'basin_relief':
+                    #    print "getting relief of: " +str(float(split_line[index]))
                     
                 #print "Finished assimilating Data Dict"
           
@@ -185,9 +190,26 @@ class CRNResults(object):
         # FIGURE 1 = ERATE COMPARED TO ERATE
         plt.figure(1, facecolor='white',figsize=(25,10))  
 
+        # get the lengths
+        #print "The lengths are: "
+        #print len(self.CRNData['AvgProdScaling'])
+        #print len(self.CRNData['Error_CR'])
+        #print len(self.CRNData['basin_relief'])
+
+        ##########################################        
         # Plot 1 = comparison new_code / cosmocalc
-        ax = plt.subplot(1,1,1)
-        plt.plot(self.CRNData['AvgProdScaling'],self.CRNData['Error_CR'],"o", markersize=8     )
+        ############################################
+        ##       
+        ax = plt.subplot(2,2,1)
+        #plt.plot(self.CRNData['AvgProdScaling'],self.CRNData['Error_CR'],"o", markersize=8     )
+        plt.scatter(self.CRNData['AvgProdScaling'],self.CRNData['Error_CR'],
+                    c=self.CRNData['basin_relief'], s=50, edgecolor='', 
+                    cmap=plt.get_cmap("autumn_r"))
+
+        cbar = plt.colorbar()
+        cbar.set_label('Basin Relief (m)')         
+        # the basin relief data is in self.CRNData['basin_relief']       
+        
         #plt.plot(self.CRNData['AvgProdScaling'],self.CRNData['Error_CR'],color=cmap(self.CRNData['basin_relief']),"o", markersize=8     )
         #plt.errorbar(datazz['erate_cosmocalc']*10, datazz['erate_cmperkyr']*10, xerr=datazz['error_cosmocalc'], yerr=datazz['error_newcode'], fmt='o',color = cmap(colo))  
         ax.spines['top'].set_linewidth(2.5)
@@ -202,6 +224,90 @@ class CRNResults(object):
         plt.title('Comparison between CRNaptious and CRONUS erosion rates',fontsize = label_size+6)
         #handles, labels = ax.get_legend_handles_labels()
         #plt.legend(handles, labels, numpoints = 1, loc='upper left')
+
+        ##########################################        
+        # Plot 2 = comparison new_code / cosmocalc
+        ############################################
+        ##  
+        ax = plt.subplot(2,2,2)
+        #plt.plot(self.CRNData['AvgProdScaling'],self.CRNData['Error_CR'],"o", markersize=8     )
+        plt.scatter(self.CRNData['AverageCombinedScaling'],self.CRNData['Error_CR_em'],
+                    c=self.CRNData['basin_relief'], s=50, edgecolor='', 
+                    cmap=plt.get_cmap("autumn_r"))
+
+        cbar = plt.colorbar()
+        cbar.set_label('Basin Relief (m)')         
+        # the basin relief data is in self.CRNData['basin_relief']       
+        
+        #plt.plot(self.CRNData['AvgProdScaling'],self.CRNData['Error_CR'],color=cmap(self.CRNData['basin_relief']),"o", markersize=8     )
+        #plt.errorbar(datazz['erate_cosmocalc']*10, datazz['erate_cmperkyr']*10, xerr=datazz['error_cosmocalc'], yerr=datazz['error_newcode'], fmt='o',color = cmap(colo))  
+        ax.spines['top'].set_linewidth(2.5)
+        ax.spines['left'].set_linewidth(2.5)
+        ax.spines['right'].set_linewidth(2.5)
+        ax.spines['bottom'].set_linewidth(2.5) 
+        ax.tick_params(axis='both', width=2.5)    
+        plt.xlabel('Erosion rate in g cm$^{-2}$ yr$^{-1}$', fontsize = axis_size-2)
+        plt.ylabel('Error between CRNAPTIOUS and CRONUS erosion rates', fontsize = axis_size-2)
+        #if zoom ==1:
+        #    plt.axis([0, 500, 0, 500])
+        plt.title('Comparison between CRNaptious and CRONUS erosion rates',fontsize = label_size+6)
+
+
+        ##########################################        
+        # Plot 3 = comparison new_code / cosmocalc
+        ############################################
+        ##  
+        ax = plt.subplot(2,2,3)
+        #plt.plot(self.CRNData['AvgProdScaling'],self.CRNData['Error_CR'],"o", markersize=8     )
+        plt.scatter(self.CRNData['AverageCombinedScaling'],self.CRNData['Error_CR'],
+                    c=self.CRNData['basin_relief'], s=50, edgecolor='', 
+                    cmap=plt.get_cmap("autumn_r"))
+
+        cbar = plt.colorbar()
+        cbar.set_label('Basin Relief (m)')         
+        # the basin relief data is in self.CRNData['basin_relief']       
+        
+        #plt.plot(self.CRNData['AvgProdScaling'],self.CRNData['Error_CR'],color=cmap(self.CRNData['basin_relief']),"o", markersize=8     )
+        #plt.errorbar(datazz['erate_cosmocalc']*10, datazz['erate_cmperkyr']*10, xerr=datazz['error_cosmocalc'], yerr=datazz['error_newcode'], fmt='o',color = cmap(colo))  
+        ax.spines['top'].set_linewidth(2.5)
+        ax.spines['left'].set_linewidth(2.5)
+        ax.spines['right'].set_linewidth(2.5)
+        ax.spines['bottom'].set_linewidth(2.5) 
+        ax.tick_params(axis='both', width=2.5)    
+        plt.xlabel('Erosion rate in g cm$^{-2}$ yr$^{-1}$', fontsize = axis_size-2)
+        plt.ylabel('Error between CRNAPTIOUS and CRONUS erosion rates', fontsize = axis_size-2)
+        #if zoom ==1:
+        #    plt.axis([0, 500, 0, 500])
+        plt.title('Comparison between CRNaptious and CRONUS erosion rates',fontsize = label_size+6)
+
+        ##########################################        
+        # Plot 4 = comparison new_code / cosmocalc
+        ############################################
+        ##  
+        ax = plt.subplot(2,2,4)
+        #plt.plot(self.CRNData['AvgProdScaling'],self.CRNData['Error_CR'],"o", markersize=8     )
+        plt.scatter(self.CRNData['AverageShielding'],self.CRNData['Error_CR'],
+                    c=self.CRNData['basin_relief'], s=50, edgecolor='', 
+                    cmap=plt.get_cmap("autumn_r"))
+
+        cbar = plt.colorbar()
+        cbar.set_label('Basin Relief (m)')         
+        # the basin relief data is in self.CRNData['basin_relief']       
+        
+        #plt.plot(self.CRNData['AvgProdScaling'],self.CRNData['Error_CR'],color=cmap(self.CRNData['basin_relief']),"o", markersize=8     )
+        #plt.errorbar(datazz['erate_cosmocalc']*10, datazz['erate_cmperkyr']*10, xerr=datazz['error_cosmocalc'], yerr=datazz['error_newcode'], fmt='o',color = cmap(colo))  
+        ax.spines['top'].set_linewidth(2.5)
+        ax.spines['left'].set_linewidth(2.5)
+        ax.spines['right'].set_linewidth(2.5)
+        ax.spines['bottom'].set_linewidth(2.5) 
+        ax.tick_params(axis='both', width=2.5)    
+        plt.xlabel('Erosion rate in g cm$^{-2}$ yr$^{-1}$', fontsize = axis_size-2)
+        plt.ylabel('Error between CRNAPTIOUS and CRONUS erosion rates', fontsize = axis_size-2)
+        #if zoom ==1:
+        #    plt.axis([0, 500, 0, 500])
+        plt.title('Comparison between CRNaptious and CRONUS erosion rates',fontsize = label_size+6)
+
+
         plt.show()
             
             
