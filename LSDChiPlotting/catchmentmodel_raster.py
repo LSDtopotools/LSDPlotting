2e4dd1a266f1ce565266c6bb7799db5efb0950e2
decvalts <declan.valters@manchester.ac.uk>
2016-07-20 18:43:19 +0100

    Added a plot script for plotting catchment model rasters and drapes

---
+++ LSDChiPlotting/catchmentmodel_raster.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 11 10:50:27 2016
+
+Catchment Model plotter
+
+This tool is to plot the output from the Catchment Model
+as a series of rasters and overlays of different data,
+e.g. Hillshade overlaid by erosion/deposition amounts, or water depth
+or d50 size, etc.
+
+@author: dav
+"""
+
+import LSDPlottingTools as lsdplt
+
+
+"""Plots a hillshade overlain with the erossion or deposition amount"""
+
+folder = "/mnt/DATA/DATA/VIRTUALBOX_SHARED/HydrogeomorphPaper/BOSCASTLE/PaperSimulations/Lumped/TransportLim/"
+laptop_folder = ""
+
+filename = "boscastle5m_bedrock_fill.asc"
+drapename = "elevdiff4320.txt"
+
+lsdplt.DrapedOverHillshade(folder + filename, folder + drapename)
+
+ 
