9ba9a9f40777652fa1e81e3c260c4f75af619881
decvalts <declan.valters@manchester.ac.uk>
2016-09-11 09:59:24 +0100

    Added some logic for single plots vs multiple plots

--- CatchmentModel_metrics/CatchmentModel_metrics.py
+++ CatchmentModel_metrics/CatchmentModel_metrics.py
@@ -27,7 +27,8 @@ from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 
 
 # VARIABLES
-data_dir = "/mnt/SCRATCH/Analyses/HydrogeomorphPaper/BOSCASTLE/Analysis/"
+#data_dir = "/mnt/SCRATCH/Analyses/HydrogeomorphPaper/BOSCASTLE/Analysis/"
+data_dir = "/run/media/dav/SHETLAND/Analyses/HydrogeomorphPaper/BOSCASTLE/Analysis/"
 #data_dir="/mnt/WORK/Dev/PyToolsPhD/Radardata_tools"
 input_raster2 = "elev.txt"
 input_raster1 = "elevdiff.txt"
@@ -62,27 +63,47 @@ def plot_scatter_rasterdata(data_array1, data_array2):
     plt.ylabel("Elevation difference (mm)")
     plt.show()
     
-def plot_hydrograph(data_dir,timeseries,axes):
-        
+def plot_hydrograph(data_dir,timeseries, axes=None, ax_inset=None):   
+
     filename = data_dir + timeseries
     time_step, q_lisflood, q_topmodel, sed_tot = np.loadtxt(filename, usecols=(0,1,2,4), unpack=True)
     
+    if axes==None:
+        fig, ax = plt.subplots()
+        
     axes.plot(time_step, q_lisflood)
     axes.set_xlim(450,600)
-    #plt.plot(time_step, q_topmodel)    
-    #plot_inset(axes, time_step, q_lisflood)
+
+    if ax_inset==True:
+      plot_inset(axes, time_step, q_lisflood, ax_inset)
     
-def plot_ensemble_hydrograph(data_dir, wildcard_fname):
+
+    
+def plot_ensemble_hydrograph(data_dir, wildcard_fname, inset=False):
     fig, ax = plt.subplots()
+    
+    if inset==True:
+      ax_inset = zoomed_inset_axes(ax, 2, loc=1)
+          # hide every other tick label
+      for label in ax_inset.get_xticklabels()[::2]:
+        label.set_visible(False)
+    else:
+      ax_inset=None
+
     # Loop through the files in a dir and plot them all on the same axes
     for f in glob.glob(data_dir + wildcard_fname):
         current_timeseries = os.path.basename(f)
         print current_timeseries
-        plot_hydrograph(data_dir, current_timeseries, ax)
-        
-def plot_inset(parent_axes, x_data, y_data):
-    ax_inset = zoomed_inset_axes(parent_axes, 1, loc=1)
+        plot_hydrograph(data_dir, current_timeseries, ax, ax_inset)
+    
+    # draw a bbox of the region of the inset axes in the parent axes and
+    # connecting lines between the bbox and the inset axes area
+    #mark_inset(ax, ax_inset, loc1=2, loc2=3, fc="none", ec="0.5")
+            
+def plot_inset(parent_axes, x_data, y_data, ax_inset):
     ax_inset.plot(x_data, y_data)
+    ax_inset.set_xlim(470,500)
+    ax_inset.set_ylim(100,160)
 
     
     
@@ -92,7 +113,7 @@ def plot_inset(parent_axes, x_data, y_data):
 
 #plot_hydrograph(data_dir,timeseries)
 
-plot_ensemble_hydrograph(data_dir, wildcard_fname)
+plot_ensemble_hydrograph(data_dir, wildcard_fname, inset=True)
 
 
 
