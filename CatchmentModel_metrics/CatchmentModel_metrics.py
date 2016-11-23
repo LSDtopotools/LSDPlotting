80e445b48f200f0d34a590a685ccc2b71cf09e5e
decvalts <declan.valters@gmail.com>
2016-11-23 20:25:24 +0000

    Additions to catchment model timeseries plotter module

--- CatchmentModel_metrics/CatchmentModel_metrics.py
+++ CatchmentModel_metrics/CatchmentModel_metrics.py
@@ -2,16 +2,10 @@
 """
 Created on Thu Jun 16 12:08:22 2016
 
-Erosion scatter plots
+Caesar Lisflood plotting tools
 
-This script plots erosion amount against other metrics,
-such as elevation and precipitation.
-
-Actually, it can be used as a generic DEM vs DEM plotter:
-You just need to supply 2 input dems/raster data files of
-the same dimension and resolution. It will plot each pixel
-as an x y scatter plot. Function for doing binned/window
-averages also under construction.
+This module is for plotting output fromt he CAESAR-Lisflood model timeseries files
+(i.e. the .dat file from a simulation, if you've stuck with the default naming convention.)
 
 @author: dav
 """
@@ -54,18 +48,38 @@ def plot_scatter_rasterdata(data_array1, data_array2):
     plt.show()
 
 
-def convert_timestep(time_step):
-    minutes = time_step*5
+def convert_timestep(time_step, time_delta, time_label):
+    # Supported time formats: minutes, hours, days, years
+
+
+
+    minutes = time_step*time_delta
     hours = minutes/60
-    return hours
+    days = hours/24
+    years = days/365.25
+
+    dic = {'minutes': minutes,
+           'hours': hours,
+           'days': days,
+           'years': years
+           }
+
+    if time_label not in dic:
+      raise ValueError( "Your selected time units are not supported: %s" % time_label )
+
+    return dic[time_label]
 
 
 class CaesarTimeseriesPlot(object):
 
-    def __init__(self, data_dir, fname, datametric):
+    def __init__(self, data_dir, fname, datametric, time_delta=60, time_label='hours',
+                 colormap='jet'):
         self.data_dir = data_dir
         self.fname = fname
         self.fig, self.ax = plt.subplots()
+        self.time_delta = time_delta # timestep in the .dat file
+        self.time_label = time_label # Units that will appear on the graph
+        self.colormap = colormap
 
         self.ax_inset = None
         self.metric_name = datametric
@@ -84,6 +98,7 @@ class CaesarTimeseriesPlot(object):
                                     usecols=(0, 1, 2, 4, 5, 6, 7,
                                              8, 9, 10, 11, 12, 13),
                                              unpack=True)
+        cumulative_sed_tot = np.cumsum(sed_tot)
 
         # Create a dictionary to store the keys/arrays
         # You can add to this later without having to modify
@@ -100,6 +115,8 @@ class CaesarTimeseriesPlot(object):
                'd7': d7,
                'd8': d8,
                'd9': d9,
+               # Special calls (not in raw data file)
+               'cumulative_sed_tot': cumulative_sed_tot,
                }
         return time_step, dic[data_name]
 
@@ -114,7 +131,7 @@ class CaesarTimeseriesPlot(object):
         # get the time step array and the data metric
         # you want to plot against it.
         time_step, metric = self.get_datametric_array(filename, self.metric_name)
-        hours = convert_timestep(time_step)
+        time_units = convert_timestep(time_step, self.time_delta, self.time_label)
 
         # If we want to draw the inset,
         # and we haven't already got an inset axes
@@ -124,25 +141,25 @@ class CaesarTimeseriesPlot(object):
             self.ax_inset = self.create_inset_axes()
 
         # plot the main axes data
-        line, = self.ax.plot(hours, metric, linewidth=1)
+        line, = self.ax.plot(time_units, metric, linewidth=1.5, alpha=0.5)
         line.set_label(line_label)
         #labelLine(line, 45)
 
         # Tweak the xlimits to zone in on the relevnat bit of hydrograph
-        self.ax.set_xlim(0, 400)
-        self.ax.set_ylim(0,60)
+        #self.ax.set_xlim(0, 900)
+        #self.ax.set_ylim(0,60)
 
         # Now plot the inset data
         if self.ax_inset is not None:
-            self.plot_inset(hours, metric)
+            self.plot_inset(time_units, metric)
 
 
 
     def plot_ensemble_hydrograph(self, draw_inset=False, labellines=False):
 
-        cm = plt.get_cmap('jet')
+        cm = plt.get_cmap(self.colormap)
         self.ax.set_color_cycle([cm(1.*i/self.num_graphs)
-                                for i in range(self.num_graphs)])
+                               for i in range(self.num_graphs)])
 
         if draw_inset is True:
             self.ax_inset = self.create_inset_axes()
@@ -180,9 +197,12 @@ class CaesarTimeseriesPlot(object):
           'd7': "d7 proportion",
           'd8': "d8 proportion",
           'd9': "d9 proportion",
+          'cumulative_sed_tot' : "Cumulative sediment yield ($m^3$)",
         }
 
-        self.ax.set_xlabel("simulation time (hours)")
+        time_axis_label = 'Simulated time (' + str(self.time_label)  + ')'
+
+        self.ax.set_xlabel(time_axis_label)
         self.ax.set_ylabel(label_dic[self.metric_name])
 
     def make_line_label(self, fname):
@@ -205,27 +225,29 @@ class CaesarTimeseriesPlot(object):
         self.fig.savefig(save_name, bbox_inches='tight')
 
     def create_inset_axes(self):
-        ax_inset = zoomed_inset_axes(self.ax, 2, loc=1)
+        ax_inset = zoomed_inset_axes(self.ax, 3.5, loc=1)
 
-        cm = plt.get_cmap('winter')
+        cm = plt.get_cmap(self.colormap)
         ax_inset.set_color_cycle([cm(1.*i/self.num_graphs)
                                  for i in range(self.num_graphs)])
 
         # hide every other tick label
-        for label in ax_inset.get_xticklabels()[::4]:
+        for label in ax_inset.get_xticklabels()[::2]:
             label.set_visible(False)
 
         return ax_inset
 
     def plot_inset(self, x_data, y_data):
 
-        self.ax_inset.plot(x_data, y_data)
-        self.ax_inset.set_xlim(39, 41)
-        self.ax_inset.set_ylim(0, 100)
+        self.ax_inset.plot(x_data, y_data,alpha=0.5)
+        # SHould be user settable
+        self.ax_inset.set_xlim(40, 70)
+        self.ax_inset.set_ylim(5, 30)
 
     def plot_legend(self):
-        self.ax.legend(bbox_to_anchor=(1, 0.75),
-               loc='center left', prop={'size': 11})
+        self.ax.legend(#bbox_to_anchor=(1, 0.4),
+               #loc='center left', prop={'size': 11})
+               loc=2, prop={'size': 11})
 
     def showfig(self):
         """
@@ -250,7 +272,7 @@ class CaesarTimeseriesPlot(object):
 
 # VARIABLES
 #data_dir = "/mnt/SCRATCH/Analyses/HydrogeomorphPaper/"
-data_dir = "/mnt/SCRATCH/Analyses/RegressionTest/"
+data_dir = "/mnt/SCRATCH/Analyses/RegressionTest/final_regression_test_intel_gcc/"
 #data_dir = "/run/media/dav/SHETLAND/Analyses/HydrogeomorphPaper/BOSCASTLE/Analysis/"
 #data_dir="/mnt/WORK/Dev/PyToolsPhD/Radardata_tools"
 #data_dir = "/mnt/SCRATCH/Analyses/Topmodel_Sensitivity/Ryedale_storms_calibrate/"
@@ -274,14 +296,22 @@ external_file_data = "Ryedale72hours_measured.csv"
 
 #plot_hydrograph(data_dir, fname, "q_lisflood", draw_inset=True)
 
+time_delta = 60
+# time delta is the difference between output time steps in the .dat file (minutes)
+# You must know this beforehand, i.e. it can't be calculated from the timeseries
+# file alone.
 
 # OOP way
-SwaleHydroS = CaesarTimeseriesPlot(data_dir, "*.dat", "sed_tot")
-SwaleHydroS.plot_ensemble_hydrograph(draw_inset=False, labellines=False)
+SwaleHydroS = CaesarTimeseriesPlot(data_dir, "*.dat", "sed_tot",
+                                   time_delta=time_delta, time_label='days',
+                                   colormap='gnuplot2')
+
+SwaleHydroS.plot_ensemble_hydrograph(draw_inset=True, labellines=False)
 #BosHydroS.plot_external_data(data_dir + external_file_data)
 SwaleHydroS.plot_legend()
 #SwaleHydroS.save_figure("TEST.svg")
-
+SwaleHydroS.ax.set_xlim(0,250)
+SwaleHydroS.ax.set_ylim(0,200)
 # ax.set_color_cycle([cm(1.*i/NUM_COLORS) for i in range(NUM_COLORS)])
 
 
